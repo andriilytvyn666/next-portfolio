8cb44178c0dc76df32a1231c28aa3597b88411dd
Andrii Lytvyn <lytvyn349@gmail.com>
2023-03-25 18:47:18 +0200

    fix: truncate card text on mobile devices

--- next/app/components/Card.tsx
+++ next/app/components/Card.tsx
@@ -14,18 +14,17 @@ export default function Card({ title, subtitle, url, imageUrl }: Props) {
   return (
     <Link href={url} target="_blank" className="w-full h-20">
       <div className="flex gap-4 p-4 rounded-lg hover:bg-bg-active">
-        <div className="w-12 h-12 overflow-hidden rounded-lg">
-          <Image
-            unoptimized
-            src={imageUrl}
-            width="48"
-            height="48"
-            alt="rihter-logo"
-          />
-        </div>
-        <div className="flex flex-col gap-1">
-          <h4 className="text-fg">{title}</h4>
-          <h5 className="text-fg-secondary">{subtitle}</h5>
+        <Image
+          unoptimized
+          src={imageUrl}
+          width="48"
+          height="48"
+          className="w-12 h-12 rounded-lg min-w-12 min-h-12"
+          alt="rihter-logo"
+        />
+        <div className="flex flex-col gap-1 truncate">
+          <h4 className="truncate text-fg">{title}</h4>
+          <h5 className="truncate text-fg-secondary">{subtitle}</h5>
         </div>
       </div>
     </Link>
--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -1,91 +1,91 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     fontSize: {
       itemTitle: [
-        "1.125rem",
+        '1.125rem',
         {
-          lineHeight: "1.375rem",
-          fontWeight: "500",
+          lineHeight: '1.375rem',
+          fontWeight: '500',
         },
       ],
       itemSubtitle: [
-        "1rem",
+        '1rem',
         {
-          lineHeight: "1.375rem",
-          fontWeight: "500",
+          lineHeight: '1.375rem',
+          fontWeight: '500',
         },
       ],
       itemNav: [
-        "1rem",
+        '1rem',
         {
-          lineHeight: "1.75rem",
-          fontWeight: "500",
+          lineHeight: '1.75rem',
+          fontWeight: '500',
         },
       ],
       footer: [
-        "1rem",
+        '1rem',
         {
-          lineHeight: "1.5rem",
-          fontWeight: "500",
+          lineHeight: '1.5rem',
+          fontWeight: '500',
         },
       ],
       body: [
-        "1rem",
+        '1rem',
         {
-          lineHeight: "1.5rem",
-          fontWeight: "500",
+          lineHeight: '1.5rem',
+          fontWeight: '500',
         },
       ],
       header: [
-        "2rem",
+        '2rem',
         {
-          lineHeight: "2.375rem",
-          fontWeight: "700",
+          lineHeight: '2.375rem',
+          fontWeight: '700',
         },
       ],
       headerXl: [
-        "2.25rem",
+        '2.25rem',
         {
-          lineHeight: "2.5rem",
-          fontWeight: "700",
+          lineHeight: '2.5rem',
+          fontWeight: '700',
         },
       ],
       headerSm: [
-        "1.25rem",
+        '1.25rem',
         {
-          lineHeight: "1.75rem",
-          fontWeight: "600",
+          lineHeight: '1.75rem',
+          fontWeight: '600',
         },
       ],
       skillChip: [
-        "1rem",
+        '1rem',
         {
-          lineHeight: "1.5rem",
-          fontWeight: "400",
+          lineHeight: '1.5rem',
+          fontWeight: '400',
         },
       ],
     },
     colors: {
       bg: {
-        DEFAULT: "#050505",
-        active: "#212121",
+        DEFAULT: '#050505',
+        active: '#212121',
       },
       fg: {
-        DEFAULT: "#FFFFFF",
-        secondary: "#838383",
+        DEFAULT: '#FFFFFF',
+        secondary: '#838383',
       },
     },
     extend: {
       screens: {
-        xs: "320px",
+        xs: '320px',
       },
     },
   },
   plugins: [],
-};
+}
