d350c202396b0bbf3e79264e49b733d20ace9e6a
Andrii Lytvyn <lytvyn349@gmail.com>
2023-08-04 06:14:15 +0300

    feat: update buttons layout on homepage

--- next/app/components/ButtonLink.tsx
+++ next/app/components/ButtonLink.tsx
@@ -1,20 +1,52 @@
 import Link from 'next/link'
 import React from 'react'
+import { ReactNode } from 'react'
+import { BsFillFilePdfFill } from 'react-icons/bs'
+import { FiArrowUpRight } from 'react-icons/fi'
+import { MdEmail } from 'react-icons/md'
+import { RiFileWord2Fill } from 'react-icons/ri'
+import { SiGithub, SiLinkedin, SiSkype, SiTelegram } from 'react-icons/si'
 
 import styles from './ButtonLink.module.css'
 
 type Props = {
   title: string
   link: string
-  children: React.ReactNode
 }
 
-export default function ButtonLink({ title, children, link }: Props) {
+const selectIcon = (name: string): ReactNode => {
+  switch (name) {
+    case 'Github':
+      return <SiGithub className="w-full h-full fill-fg" />
+    case 'Linkedin':
+      return <SiLinkedin className="w-full h-full fill-fg" />
+    case 'Email':
+      return <MdEmail className="w-full h-full fill-fg" />
+    case 'Skype':
+      return <SiSkype className="w-full h-full fill-fg" />
+    case 'Telegram':
+      return <SiTelegram className="w-full h-full fill-fg" />
+    case 'Resume':
+      return <BsFillFilePdfFill className="w-full h-full fill-bg" />
+    default:
+      return <FiArrowUpRight className="w-full h-full fill-fg" />
+  }
+}
+
+export default function ButtonLink({ title, link }: Props) {
   return (
-    <Link href={link} target="_blank">
-      <button className={styles['button-link']}>
-        <div className="w-5 h-5">{children}</div>
-        <span className="text-fg">{title}</span>
+    <Link
+      href={link}
+      target="_blank"
+      className={`${title === 'Resume' ? 'ml-auto' : ''}`}
+    >
+      <button
+        className={`${styles['button-link']} ${
+          title == 'Resume' ? 'px-4 bg-fg hover:bg-fg-secondary' : 'px-3'
+        }`}
+      >
+        <div className="w-5 h-5">{selectIcon(title)}</div>
+        {title === 'Resume' ? <span className="text-bg">{title}</span> : ''}
       </button>
     </Link>
   )
--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,11 +1,5 @@
 import { PortableText, PortableTextComponents } from '@portabletext/react'
 import Link from 'next/link'
-import { ReactNode } from 'react'
-import { BsFillFilePdfFill } from 'react-icons/bs'
-import { FiArrowUpRight } from 'react-icons/fi'
-import { MdEmail } from 'react-icons/md'
-import { RiFileWord2Fill } from 'react-icons/ri'
-import { SiGithub, SiLinkedin, SiSkype, SiTelegram } from 'react-icons/si'
 
 import client from '../client'
 import ButtonLink from './components/ButtonLink'
@@ -43,27 +37,6 @@ export default async function Home() {
     },
   }
 
-  const selectIcon = (name: string): ReactNode => {
-    switch (name) {
-      case 'Github':
-        return <SiGithub className="w-full h-full fill-fg" />
-      case 'Linkedin':
-        return <SiLinkedin className="w-full h-full fill-fg" />
-      case 'Email':
-        return <MdEmail className="w-full h-full fill-fg" />
-      case 'Skype':
-        return <SiSkype className="w-full h-full fill-fg" />
-      case 'Telegram':
-        return <SiTelegram className="w-full h-full fill-fg" />
-      case 'Resume.pdf':
-        return <BsFillFilePdfFill className="w-full h-full fill-fg" />
-      case 'Resume.docx':
-        return <RiFileWord2Fill className="w-full h-full fill-fg" />
-      default:
-        return <FiArrowUpRight className="w-full h-full fill-fg" />
-    }
-  }
-
   return (
     <>
       <h1 className="text-center text-headerXl text-fg md:text-left">
@@ -73,13 +46,9 @@ export default async function Home() {
         <PortableText value={data.text} components={portableTextComponents} />
       </div>
 
-      <div className="grid gap-4 sm:flex sm:flex-wrap">
+      <div className="flex flex-wrap gap-4">
         {data.links.map((link, index) => {
-          return (
-            <ButtonLink key={index} title={link.name} link={link.link}>
-              {selectIcon(link.name)}
-            </ButtonLink>
-          )
+          return <ButtonLink key={index} title={link.name} link={link.link} />
         })}
       </div>
     </>
