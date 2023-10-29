632ef0af08ccbc986d471203230d4483878745e9
Andrii Lytvyn <lytvyn349@gmail.com>
2023-10-29 10:51:07 +0200

    feat: add resume page

---
+++ next/app/resume/page.tsx
@@ -0,0 +1,8 @@
+export default function Resume() {
+  return (
+    <iframe
+      src="/andrii-lytvyn-resume.pdf#zoom=100&topOffset=30"
+      className="max-w-[90rem] w-full min-h-[75rem]"
+    ></iframe>
+  )
+}
