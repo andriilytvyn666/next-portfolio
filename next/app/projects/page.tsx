775683ab7b824eaf26082e1bc075cff69c48619a
Andrii Lytvyn <lytvyn349@gmail.com>
2023-11-08 10:37:10 +0200

    refactor: adjust pix and projects grids

--- next/app/projects/page.tsx
+++ next/app/projects/page.tsx
@@ -14,7 +14,7 @@ export default async function Projects() {
 
   return (
     <div className="flex flex-col-reverse gap-8 lg:flex-col">
-      <Tabs className="sticky bottom-4 lg:top-[5.75rem] flex" />
+      <Tabs className="sticky bottom-4 lg:bottom-0 lg:top-[5.75rem] flex" />
       <div className="grid mx-auto lg:grid-cols-4 items-centerlg gap-y-8 gap-x-6 w-fit">
         <ProjectsGrid projectGroups={data} />
         <ProjectsGrid projectGroups={data} />
