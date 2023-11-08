1b5033804d3c536f1d2919af1a9ed83ba6721fb3
Andrii Lytvyn <lytvyn349@gmail.com>
2023-11-08 13:01:06 +0200

    refactor: remove resume page

--- next/app/resume/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Resume() {
-  return (
-    <iframe
-      src="/andrii-lytvyn-resume.pdf#zoom=100&topOffset=30"
-      className="max-w-[90rem] w-full min-h-[27rem] lg:min-h-[75rem]"
-    ></iframe>
-  )
-}
