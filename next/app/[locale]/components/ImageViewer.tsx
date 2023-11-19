dde7560b949fee9619e0e83b0169d390d3349ee3
Andrii Lytvyn <lytvyn349@gmail.com>
2023-11-19 17:59:23 +0200

    fix: disable brokem modal components

--- next/app/[locale]/components/ImageViewer.tsx
+++ next/app/[locale]/components/ImageViewer.tsx
@@ -10,24 +10,18 @@ const ImageViewer: FC = () => {
   const { image, isImageOpen, setIsImageOpen } = useImageViewer()
 
   return (
-    // TODO:  review how to control modal components and refactor
-    <Modal isOpen={isImageOpen}>
-      <div
-        className={`${
-          isImageOpen ? '' : ''
-        } fixed top-0 left-0 z-50 flex items-center justify-center w-full min-h-screen px-4 lg:px-0 md:w-screen`}
-        onClick={() => setIsImageOpen(false)}
-      >
-        <div className="absolute z-20 w-full h-full bg-bg opacity-95" />
-        <Image
-          src={image}
-          alt="test"
-          width={800}
-          height={600}
-          className="z-30"
-        />
-      </div>
-    </Modal>
+    // TODO: figure out why modal is broken in webkit
+    // <Modal isOpen={isImageOpen}>
+    <div
+      className={`${
+        isImageOpen ? '' : ''
+      } fixed top-0 left-0 z-50 flex items-center justify-center w-full min-h-screen px-4 lg:px-0 md:w-screen`}
+      onClick={() => setIsImageOpen(false)}
+    >
+      <div className="absolute z-20 w-full h-full bg-bg opacity-95" />
+      <Image src={image} alt="test" width={800} height={600} className="z-30" />
+    </div>
+    // </Modal>
   )
 }
 
